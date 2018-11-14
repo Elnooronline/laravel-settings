c38ca33244a0c8192c958eea2d2cf1bf2473128f
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-11-14 11:30:25 +0200

    testing

---
+++ tests/Unit/LaravelSettingsUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Elnooronline\LaravelSettings\Facades\Setting;
+
+class LaravelSettingsUnitTest extends TestCase
+{
+    /** @test */
+    public function it_can_set_and_get_data()
+    {
+        Setting::set('name', 'Ahmed Fathy');
+        Setting::set('phone', '021207687151');
+
+        $this->assertEquals(Setting::get('phone'), '021207687151');
+        $this->assertEquals(Setting::get('name'), 'Ahmed Fathy');
+    }
+}
