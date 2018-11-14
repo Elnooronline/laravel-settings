650251dfcbc6d72b3ad98a5f9a9c3e345b77c6b1
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-11-14 13:05:57 +0200

    docs

--- config/laravel_settings.php
+++ config/laravel_settings.php
@@ -1,6 +1,20 @@
 <?php
 return [
+    /**
+     * At the moment the supported drivers only database.
+     */
     'driver' => env('SETTINGS_DRIVER', 'database'),
 
+    /**
+     * The model of the settings table.
+     * if you want to override the model you should extend your settings model from the package model class.
+     *
+     *   'model_class' => \App\CustomSetting::class,
+     *
+     *   class CustomSetting extends \Elnooronline\LaravelSettings\Models\SettingModel
+     *   {
+     *      ...
+     *   }
+     */
     'model_class' => \Elnooronline\LaravelSettings\Models\SettingModel::class,
 ];
