1330a26ffc67d41582a405f3e5fc9d2f436317bd
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-11-15 12:22:38 +0200

    add ide helpers for autocomplete

--- src/Facades/Setting.php
+++ src/Facades/Setting.php
@@ -4,6 +4,22 @@ namespace Elnooronline\LaravelSettings\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Setting
+ *
+ * @method static \Illuminate\Database\Eloquent\Model set($key, $value = null)
+ * @method static mixed get($key, $default = null)
+ * @method static \Elnooronline\LaravelSettings\Models\SettingModel getModel($key = null)
+ * @method static \Illuminate\Database\Eloquent\Collection getCollection()
+ * @method static \Illuminate\Database\Eloquent\Collection all()
+ * @method static void forget($key)
+ * @method static void forgetAll($key)
+ * @method static \Elnooronline\LaravelSettings\SettingBuilder withoutPrefix()
+ * @method static \Elnooronline\LaravelSettings\SettingBuilder setConditions(array $conditions = [])
+ * @method static \Elnooronline\LaravelSettings\SettingBuilder lang($lang = null)
+ * @method static \Elnooronline\LaravelSettings\SettingBuilder registerPrefixMethod($method)
+ * @package Elnooronline\LaravelSettings\Facades
+ */
 class Setting extends Facade
 {
     public static function getFacadeAccessor()
